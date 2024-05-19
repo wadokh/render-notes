15d46b008805bb492092648f2890797ca4044f7c
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-19 20:59:52 +0530

    add

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
