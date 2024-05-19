6d89d89f8bfad82712be16bafb761c585fffd192
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-19 19:59:49 +0530

    add

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 let notes = [
     {
@@ -75,6 +88,8 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
