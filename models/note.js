f94d5d6be700494d411ed8fb3ef91bcdef817af0
wadokh <115899643+wadokh@users.noreply.github.com>
2024-06-22 23:16:12 +0530

    restructure application

--- models/note.js
+++ models/note.js
@@ -1,27 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -33,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
